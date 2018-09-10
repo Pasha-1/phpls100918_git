f4f1e31dcbb93c5301d4f848fda9ed3dac0952b7
Pavel Tolmaci (Tolmach) <pavel.tolmaci@gmail.com>
2018-09-10 20:00:28 +0300

    2

--- picture.php
+++ picture.php
@@ -1,6 +1,8 @@
 <?php
 require_once 'vendor/autoload.php';
+
 use Intervention\Image\ImageManagerStatic as Image;
+
 $image = Image::make('acc.jpg')->widen(300);
 $image->rotate(45);
 $image->save('img/ar.jpg', 60);
