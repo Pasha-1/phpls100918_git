7bdcb623124e12b2840953f8bcb30ccb3f17fe44
Pasha-1 <42351874+Pasha-1@users.noreply.github.com>
2018-09-25 08:45:06 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -3,13 +3,13 @@ require_once 'vendor/autoload.php';
 // Create the Transport
 $transport = (new Swift_SmtpTransport('smtp.gmail.com', 465, 'ssl'))
     ->setUsername('PHGBTU.FOUNDATION@gmail.com')
-    ->setPassword('phuket@2018');
+    ->setPassword('@2018');
 // Create the Mailer using your created Transport
 $mailer = new Swift_Mailer($transport);
 // Create a message
 $message = (new Swift_Message('Wonderful Subject'))
-    ->setFrom(['john@doe.com' => 'John Doe'])
-    ->setTo(['dmitriy.shaydurov@gmail.com'])
+    ->setFrom(['@doe.com' => 'John Doe'])
+    ->setTo(['@gmail.com'])
     ->setBody('Here is the message itself');
 // Send the message
 $result = $mailer->send($message);
